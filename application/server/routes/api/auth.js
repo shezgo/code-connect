84b5dc60efde712d62bc5543c2e842311fed26fa
shezpc <shezpc@gmail.com>
2024-07-09 11:18:45 -0700

    debug

--- application/server/routes/api/auth.js
+++ application/server/routes/api/auth.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const signup_controller = require("../../controllers/signupController");
+const signupController = require("../../controllers/signupController");
 const loginController = require('../controllers/loginController');
 const router = express.Router();
 
-<<<<<<< HEAD
+
 router.get("/verify/:token", signupController.verify_email_get);
 router.post("/verify/:email", signupController.verify_email_post);
 router.post("/signup", signupController.signup_user_post);
@@ -12,13 +12,7 @@ router.post('/login', loginController.login_user_post);
 // New routes for password reset
 router.post('/forgot-password', loginController.forgot_password_post);
 router.post('/reset-password', loginController.reset_password_post);
-=======
-router.get("/verify/:token", signup_controller.verify_email_get);
-router.post("/verify/:email", signup_controller.verify_email_post);
-router.post("/signup", signup_controller.signup_user_post);
 
-router.post('/login', loginController.login_post);
->>>>>>> origin/shez-backend-dev
 
 module.exports = router;
 
