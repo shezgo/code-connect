8075850f6336e73ad450b58a50635f2dd3b5d3bc
mshigeyoshi <mshigeyoshi@sfsu.edu>
2024-07-17 19:55:02 -0700

    button triggers script

--- application/server/routes/api/search.js
+++ application/server/routes/api/search.js
@@ -1,9 +1,4 @@
-const express = require('express');
-const search_controller = require("../../controllers/searchController");
-const mysql = require('mysql8');
-
-const app = express();
-const port = 3306;
-
-const router = express.Router();
-router.get()
+// JavaScript source code
+async function searchUser(term) {
+    console.log("We are inside search.js");
+}
