ee2f6375fbaad1324addadb565123245a120fcc8
william <william910122@gmail.com>
2024-07-20 12:15:13 -0700

    post and message model/table added

---
+++ application/server/db/models/message.js
@@ -0,0 +1,39 @@
+const {DataTypes,Model} = require('sequelize');
+
+const sequelize = require("../config.js");
+
+//import for 
+const {User}=require('./user.js')
+
+class Message extends Model {}
+
+Message.init(
+    {
+        meetingID : {
+            field: 'meetingID',
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },userID: { 
+            type: DataTypes.INTEGER, 
+            // references: { 
+            //     model: User, 
+            //     key: 'userID' 
+            // }
+        },date:{
+            type:DataTypes.DATE,
+        },title:{
+            type:DataTypes.STRING(256),
+        },invitees:{
+            type:DataTypes.JSON,
+        }
+    },
+    {
+        sequelize,
+        timestamps:false,
+        modelName: 'Message',
+        tableName: 'message',
+    }
+);
+
+module.exports = Message;
---
+++ application/server/db/models/post.js
@@ -0,0 +1,42 @@
+const {DataTypes,Model} = require('sequelize');
+
+const sequelize = require("../config.js");
+
+//import for 
+const {User}=require('./user.js')
+
+class Post extends Model {}
+
+Post.init(
+    {
+        postID:{
+            type:DataTypes.INTEGER,
+            primaryKey:true,
+            autoIncrement:true,
+        },userID: { 
+            type: DataTypes.INTEGER, 
+            // references: { 
+            //     model: User, 
+            //     key: 'userID' 
+            // }
+        },content:{
+            type:DataTypes.STRING(512),
+        },codeBlock:{
+            type:DataTypes.STRING(512),
+        },date:{
+            type:DataTypes.DATEONLY,
+        },time:{
+            type:DataTypes.TIME,
+        },likes:{
+            type:DataTypes.BOOLEAN,
+        }
+    },
+    {
+        sequelize,
+        timestamps:false,
+        modelName: 'Post',
+        tableName: 'post',
+    }
+);
+
+module.exports = Post;
