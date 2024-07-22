185c31afc44f34d456b039a07f754cff2dd87d86
Ghadeer320 <galbadani@sfsu.edu>
2024-07-23 01:57:43 +0300

    Implemented search util and used it for posts. Also made seed script to insert sample data for post.

--- application/server/routes/api/index.js
+++ application/server/routes/api/index.js
@@ -1,6 +1,7 @@
 const { Router } = require('express')
 const signup = require('./auth')
 const search = require('./search')
+const post = require('./post')
 //const auth = require('./auth')
 const { version } = require('../../package.json')
 
@@ -10,6 +11,7 @@ const router = Router()
 //router.use('/signup', signup)
 router.use('/auth', signup)
 router.use('/search', search)
+router.use('/post', post)
 
 router.get('/', (req, res) => {
     res.end(`API v.${version}`)
