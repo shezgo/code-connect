50488beac02b066da8d752b42bafcdf449b286bb
Ghadeer320 <galbadani@sfsu.edu>
2024-07-23 17:56:43 +0300

    Made script to stop foreign keys

--- application/server/bin/syncDatabase.js
+++ application/server/bin/syncDatabase.js
@@ -11,10 +11,14 @@ const sync_database = async ()=>{
         const {sequelize} = await initialize();
       // await sequelize.authenticate();
       // console.log('Connection to the database has been established successfully.');
-
+      // stop foreign key constraints with force
+      if(force==="true") {
+        await sequelize.query("SET foreign_key_checks = 0;")
+      } 
+      
         await sequelize.sync({ force: force==="true"? true: false });
         console.log('Database synchronized successfully.');
-
+        await sequelize.query("SET foreign_key_checks = 1;")
         // Additional sync or setup logic here if needed
     } catch (error) {
         console.error('Unable to connect to the database:', error);
