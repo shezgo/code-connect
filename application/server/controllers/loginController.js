2727bbc32460e16728354f1e4fefefa6df16bce2
shezpc <shezpc@gmail.com>
2024-07-22 18:46:43 -0700

    integrated forgot/reset pw, resolved merge conflicts

--- application/server/controllers/loginController.js
+++ application/server/controllers/loginController.js
@@ -74,7 +74,7 @@ exports.logout_user_post = asyncHandler(async (req, res, next) => {
 //Send password reset email containing reset token. Passing this token and new password
 //into reset_password_post successfully changes the password.
 const sendPasswordResetEmail = (email, token) => {
-    const resetLink = `${process.env.HOST_NAME}:${process.env.PORT}/api/auth/reset-password?token=${token}`;
+    const resetLink = `${process.env.HOST_NAME}:${process.env.PORT}/resetpassword.html?token=${token}`;
     const text = `Hi there,\n\nPlease follow the link below to reset your password:\n\n${resetLink}\n\nThanks!`;
     const subject = 'Password Reset';
 
