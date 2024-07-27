34bf17c440cfae55191bd3c65c89d48895180317
Ghadeer320 <galbadani@sfsu.edu>
2024-07-28 00:27:00 +0300

    Refined routes and params

--- application/server/controllers/postController.js
+++ application/server/controllers/postController.js
@@ -4,15 +4,13 @@ const { Post } = require("../db/models/index.js");
 const { search } = require("../utils/index.js")
 
 exports.search_post_get = asyncHandler(async (req, res, next) => {
-    const search_query = req.params.SearchTerm;
+    const search_query = req.params.searchTerm;
     // Send filter with the body
     const filter = req.body?.filter;
     const search_options = {
         search_query: search_query, // Query of the search
         search_properties: {
-            content: 1, // Weight of the column content of model post to fuzzy search in
-            //postID,
-            //userID,
+            content: 1
         }, 
         filter: filter,
         order: ["postID", "DESC"],
