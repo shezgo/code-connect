e18c6927459f289c5338a59ed0be8855b318394c
Noahhai22 <noahhai22@gmail.com>
2024-07-30 02:10:09 -0700

    working version, css renders

--- application/BetaPrototype/folder/frontend-js/workspace.js
+++
@@ -1,7 +0,0 @@
-function openSidebar() {
-    document.getElementById("mySidebar").style.width = "250px";
-  }
-
-  function closeSidebar() {
-    document.getElementById("mySidebar").style.width = "0";
-  }
