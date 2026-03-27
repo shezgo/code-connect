894b7c56de02f2b36cecff20acbfa3d05dc41209
shezgo <shezpc@gmail.com>
2026-03-26 17:52:48 -0700

    clean up unused files

--- application/server/routes/api/post(Ghadeer).js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const search_controller= require("../../controllers/postController");
-const router = express.Router();
-
-router.get("/search/:search_query", search_controller.search_post_get);
-
-module.exports = router;
